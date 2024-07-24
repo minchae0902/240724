a7affe709e06c83e8117cae3a4f09fc53739ca8e
minchae0902 <s220254@baeksin.ms.kr>
2024-07-24 10:57:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ st.title('나의 첫 번째 웹 서비스')
 name = st.text_input('이름을 입력해주세요 : ')
 menu = st.selectbox('좋아하는 음식을 선택하세요!', ['망고 빙수', '상하이 치킨버거' , '아몬드 봉봉'])
 if st.button('인삿말 생성'):
-  st.write(f'안녕하세요! {name}님! 당신이 좋아하는 음식은 {menu} 이군요! 정말 맛있겠어요
+  st.write(f'안녕하세요! {name}님! 당신이 좋아하는 음식은 {menu} 이군요! 정말 맛있겠어요!)
